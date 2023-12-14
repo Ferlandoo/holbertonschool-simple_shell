6d56bc58ae28cbc44282bc7e10528eb10c33c78b
FloriMecaj <florimecaj31@gmail.com>
2023-12-14 04:01:40 -0800

    fix betty style

--- command_path.c
+++ command_path.c
@@ -1,39 +1,48 @@
 #include "main.h"
 
+/**
+ * command_path - Finds the full path of a command
+ * @cmd: The command to find
+ * Return: The full path of the command
+ */
+
 char *command_path(char *cmd)
 {
 	char *path, *path_copy, *full_path, *token;
+	struct stat buf;
 
 	path = _getenv("PATH");
 	if (path == NULL)
 	{
-		fprintf(stderr, "Error");
+		fprintf(stderr, "Path variable not found");
 		return (NULL);
 	}
-
 	path_copy = strdup(path);
 	if (path_copy == NULL)
 	{
-		fprintf(stderr, "Error");
-                return (NULL);
+		fprintf(stderr, "Error copying path");
+		return (NULL);
 	}
 	token = strtok(path_copy, ":");
 	while (token != NULL)
 	{
-		full_path = malloc(sizeof(strlen(token) + strlen(cmd) + 2));
+		full_path = malloc(strlen(token) + strlen(cmd) + 2);
 		if (full_path == NULL)
 		{
-			fprintf(stderr, "Error");
-                	free(path_copy);
+			fprintf(stderr, "Error allocating full path");
+			free(path_copy);
 			return (NULL);
 		}
 		strcpy(full_path, token);
-		strcpy(full_path, "/");
-		strcpy(full_path, cmd);
-
-		token = strtok(NULL, ":")
+		strcat(full_path, "/");
+		strcat(full_path, cmd);
+		if (stat(full_path, &buf) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		token = strtok(NULL, ":");
 	}
-	free(full_path);
 	free(path_copy);
 	return (full_path);
 }
