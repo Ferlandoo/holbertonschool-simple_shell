7cceda486dd71fb8259a43a25315d8a4d8a031dc
Ferlandoo <contact@ferlando.me>
2023-12-18 02:04:48 -0800

    edit

--- shell.c
+++ shell.c
@@ -55,7 +55,7 @@ int command_read(char *s)
 	return (2);
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == ' ' && s[i + 1] == s[i])
+		if (s[i] == ' ' && s[i + 1] == '\n')
 			return (0);
 	}
 	if (strcmp(s, "env") == 0)
