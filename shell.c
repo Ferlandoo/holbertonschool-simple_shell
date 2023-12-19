f565a5b33bd33cb2249dec8ae15df7024df86f2e
Ferlandoo <contact@ferlando.me>
2023-12-18 18:46:49 -0800

    strcmp exit

--- shell.c
+++ shell.c
@@ -51,7 +51,7 @@ int command_read(char *s)
 	char *cmd_array[100];
 
 	if (strcmp(s, "exit") == 0)
-		exit (EXIT_SUCCESS);
+		exit (0);
 	if (strcmp(s, "env") == 0)
 		return (_printenv());
 	token = strtok(s, " ");
