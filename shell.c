f54f5aec40c72c6a3cb7682ef4f50c50ad49a48f
Ferlandoo <contact@ferlando.me>
2023-12-18 18:42:37 -0800

    edit exit status

--- shell.c
+++ shell.c
@@ -94,7 +94,12 @@ int execute(char *cmd_arr[])
 	}
 	if (pid > 0)
 	{
-		wait(&status);
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		if (WIFEXITED(status) != 0)
+			fprintf(stderr, "Error: %s: %d\n", name, WEXITSTATUS(status));
+			exit(EXIT_FAILURE);
 	}
 	else if (pid == 0)
 	{
