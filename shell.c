56ae23c35e86df3cdeed5fe4ac65d77ef6afb318
Ferlandoo <contact@ferlando.me>
2023-12-20 05:04:12 -0800

    change print

--- shell.c
+++ shell.c
@@ -43,8 +43,7 @@ int execute(char *cmd_arr[])
 	exe_path = command_path(cmd_arr[0]);
 	if (exe_path == NULL)
 	{
-		write(1, cmd_arr[0], strlen(cmd_arr[0]));
-		write(1, ": not found\n", 12);
+		fprintf(stderr, "./hsh: 1: %s: not found\n", cmd_arr[0]);
 		return (1);
 	}
 	pid = fork();
